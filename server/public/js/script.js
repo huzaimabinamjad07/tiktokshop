7d5a507d5fae3262f0ee2dd30335757c811c098d
Huzaima Bin Amjad <huzaimabinamjad07@gmail.com>
2024-03-18 03:28:27 +0500

    progress with server

---
+++ server/public/js/script.js
@@ -0,0 +1,24 @@
+const button = document.querySelector("#buy-button");
+
+button.addEventListener("click", () => {
+  fetch("/create-checkout-session", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      items: [{ id: 1, quantity: 1 }],
+    }),
+  })
+    .then((res) => {
+      if (res.ok) return res.json();
+      return res.json().then((json) => Promise.reject(json));
+    })
+    .then(({ url }) => {
+      console.log(url);
+      //   window.location = url;
+    })
+    .catch((e) => {
+      console.log(e.error);
+    });
+});
--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 require("dotenv").config();
 
 const express = require("express");
-const app = express;
+const app = express();
 
-app.request(express.json());
+app.use(express.json());
+app.use(express.static("public"));
+
+const storeItems = new Map([[1, { priceInCents: 39900, name: "Pro Package" }]]);
 
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
+
+app.post("/create-checkout-session", (req, res) => {
+  res.json("Hi");
+});
 
 app.listen(3000);
