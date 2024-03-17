223d8d55219e2020036ecf6f0d1984b2880473c0
Huzaima Bin Amjad <huzaimabinamjad07@gmail.com>
2024-03-18 03:05:59 +0500

    added .gitignore file and server file

---
+++ server/server.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express;
+
+app.request(express.json());
+
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
+
+app.listen(3000);
