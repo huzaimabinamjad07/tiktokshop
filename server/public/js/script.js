20710a2401f9b4563038bb911450d8a3a6fc4c75
Huzaima Bin Amjad <huzaimabinamjad07@gmail.com>
2024-03-18 05:09:56 +0500

    progress

--- server/public/js/script.js
+++ server/public/js/script.js
@@ -1,6 +1,33 @@
-const button = document.querySelector("#buy-button");
+const buyButton = document.querySelector("#buy-button");
 
-button.addEventListener("click", () => {
+function sendEmail() {
+  const params = {
+    from_name: document.querySelector("#name").value,
+    email_id: document.querySelector("#email").value,
+    message: document.querySelector("#message").value,
+  };
+
+  emailjs.send("service_hzcp9i6", "template_3lo5ckp", params).then(
+    function () {
+      document.querySelector("#email-message").textContent =
+        "✅ Email Sent Successfully!";
+      setTimeout(() => {
+        document.querySelector("#email-message").textContent = "";
+        document.querySelector("form").clear();
+      }, 2000);
+    },
+    function () {
+      document.querySelector("#email-message").textContent =
+        "❌ Failed to send message!";
+      setTimeout(() => {
+        document.querySelector("#email-message").textContent = "";
+        document.querySelector("form").clear();
+      }, 2000);
+    }
+  );
+}
+
+buyButton.addEventListener("click", () => {
   fetch("/create-checkout-session", {
     method: "POST",
     headers: {
